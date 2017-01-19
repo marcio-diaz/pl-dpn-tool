b73ecb681b4ac8843bbcca201efdf36ef2816a63
Marcio Diaz <marcio.diaz@gmail.com>
2017-01-19 10:47:43 +0100

    Change to parse a few more.

--- clean.py
+++ clean.py
@@ -6,13 +6,24 @@ def clean_file(filename):
     to_define = ['vmm_spinlock_t', 'u64', 'bool', 'arch_regs_t', 'vmm_rwlock_t',
                  'irq_flags_t', 'u32', 'pthread_t', 'vmm_scheduler_ctrl',
                  'virtual_addr_t', 'u8', 'virtual_size_t', 'physical_addr_t',
-                 'physical_size_t']
+                 'physical_size_t', 'atomic_t', 'vmm_iommu_fault_handler_t',
+                 'dma_addr_t', 'size_t']
     new_file_lines = ['typedef int {};'.format(t) for t in to_define]
-    skip_lines_start_with_char = ['#', '/', '*', '1']
+    skip_lines_start_with_char = ['#', '/', '1']
+    skip_lines_start_with_two_char = ['* ', '*/', '*\n']    
     skip_lines_with = ['DEFINE_PER_CPU']
-    delete_words = ['__cpuinit', '__noreturn', '__init',
+    delete_words = ['__cpuinit', '__noreturn', '__init', '__exit',
                     'VMM_DEVTREE_PATH_SEPARATOR_STRING',
-                    'struct vmm_semaphore_resource,']
+                    'struct vmm_semaphore_resource,',
+                    'VMM_EXPORT_SYMBOL\(.*\);',
+                    'VMM_DECLARE_MODULE\(.*\);',
+                    'MODULE_AUTHOR,',
+		    'MODULE_LICENSE,',
+		    'MODULE_IPRIORITY,',
+		    'MODULE_INIT,',
+		    'MODULE_EXIT\);',
+                    'VMM_DECLARE_MODULE\(MODULE_DESC,',
+                    'unsigned long addr_merge,', 'PRIPADDR']
     replace_words = {'for_each_present_cpu':'while',
                      'for_each_cpu\(.*\)':'while(1)',                     
                      'list_for_each_entry\(.*\)':'if(1)',
@@ -26,6 +37,8 @@ def clean_file(filename):
         sline = line.lstrip(' \t')
         if sline[0] in skip_lines_start_with_char:
             continue
+        if sline[:2] in skip_lines_start_with_two_char:
+            continue        
         if any([w in sline for w in skip_lines_with]):
             continue
         for w in delete_words:
