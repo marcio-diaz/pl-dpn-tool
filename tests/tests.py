735bb783fbb7ca1cfbeceedcd87e4f2ffe60563c
Marcio Diaz <marcio.diaz@gmail.com>
2017-01-30 22:42:20 +0100

    remove xvisor test

--- tests/tests.py
+++ tests/tests.py
@@ -26,21 +26,7 @@ class TestRaceDetectionOnFile(unittest.TestCase):
                       spawn_end_gamma=state.spawn_end_gamma)
         result = run_race_detection(pldpn, state.global_vars)
         self.assertFalse(result)
-        
-    def test_xvisor_core(self):
-        directory = "./tests/xvisor-0.2.8/core/"
-        c_files = [file for file in glob.glob(directory
-                                + '/**/*.c', recursive=True)]
-        state = State()
-        THREAD_NAME = "vmm_threads_create"
-        for i, filename in enumerate(c_files):
-            process_file(filename, state)
-        pldpn = PLDPN(control_states=state.control_states,
-                      gamma=state.gamma,
-                      rules=state.rules,
-                      spawn_end_gamma=state.spawn_end_gamma)
-        result = run_race_detection(pldpn, state.global_vars)
-        self.assertFalse(result)
+
         
 if __name__ == "__main__":
     unittest.main()
